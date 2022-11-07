09b1c1d300fea4dcc32b700ae1d8481d51bb6416
chan kang <chan@memorahealth.com>
2022-11-06 23:39:40 -0500

    yum

--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 set -xe
 
-# token='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'
+token='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'
 branch_name='main'
 bot_name='chan-kang'
 
-# REPO_REF="https://${bot_name}:${token}@github.com/${GITHUB_REPOSITORY}.git"
+REPO_REF="https://${bot_name}:${token}@github.com/${GITHUB_REPOSITORY}.git"
 # git config http.sslVerify false
 git config user.name "${bot_name}"
 git config user.email "${bot_name}@users.noreply.github.com"
